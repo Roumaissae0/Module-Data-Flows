4fd3d0283f087d34124d90a2ef2fc29e1c69a2e6
Roumaissae <alroumaissaebb@gmail.com>
2026-06-07 17:22:32 +0200

    Feat: tests for removeVowelsFromWords function

--- challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.test.js
+++ challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.test.js
@@ -1,11 +1,24 @@
 let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
-  // Arrange
-  // Act
-  // Assert
+  const input = ["Irina", "Etza", "Daniel"];
+  const expected = ["rn", "tz", "Dnl"];
+  const output = removeVowelsFromWords(input);
+  expect(output).toEqual(expected);
+});
+test("returns empty array if input is empty array", function () {
+  const input = [];
+  const expected = [];
+  const output = removeVowelsFromWords(input);
+  expect(output).toEqual(expected);
 });
 
+test("removes all vowels from a single word", function () {
+  const input = ["hello"];
+  const expected = ["hll"];
+  const output = removeVowelsFromWords(input);
+  expect(output).toEqual(expected);
+});
 // example
 // input: ["Irina", "Etza", "Daniel"]
 // expected output: ["rn", "tz", "Dnl"]
