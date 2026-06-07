640c995bd7be834a5712575adf5973545a668970
Roumaissae <alroumaissaebb@gmail.com>
2026-06-07 17:21:03 +0200

    Feat: tests for getLargestNumber including edge cases and array mutation check

--- challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
+++ challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
@@ -1,11 +1,31 @@
 let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
-  // Arrange
-  // Act
-  // Assert
+  const input = [3, 21, 88, 4, 36];
+  const expected = 88;
+  const output = getLargestNumber(input);
+  expect(output).toEqual(expected);
+});
+test("returns largest number in array with negative numbers", function () {
+  const input = [-3, -21, -1, -4, -36];
+  const expected = -1;
+  const output = getLargestNumber(input);
+  expect(output).toEqual(expected);
 });
 
+test("returns the only number if array has one element", function () {
+  const input = [5];
+  const expected = 5;
+  const output = getLargestNumber(input);
+  expect(output).toEqual(expected);
+});
+
+test("original array is not changed", function () {
+  const input = [3, 21, 88, 4, 36];
+  const copy = [...input];
+  getLargestNumber(input);
+  expect(input).toEqual(copy);
+});
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
