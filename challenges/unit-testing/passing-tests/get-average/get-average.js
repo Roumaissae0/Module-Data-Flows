7d1a79df18cd5c6c29b4873359a3446984e8ee6d
Roumaissae <alroumaissaebb@gmail.com>
2026-06-07 17:19:37 +0200

    Feat: average function filters strings and returns average of numbers

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,10 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+     const onlyNumbers = numbers.filter((item) => typeof item === "number");
+  const sum = onlyNumbers.reduce((total, num) => total + num, 0);
+  return sum / onlyNumbers.length;
+}
 
 module.exports = average;
