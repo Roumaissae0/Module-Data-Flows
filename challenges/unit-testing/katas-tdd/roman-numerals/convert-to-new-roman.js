8618d848ff0b9c5132ec6d24fac08207e6e54015
Roumaissae <alroumaissaebb@gmail.com>
2026-06-07 17:04:25 +0200

    Feat: convertToNewRoman converts arabic numbers to new roman numerals

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,3 +1,29 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(number) { const values = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+
+  let result = "";
+
+  for (const [value, numeral] of values) {
+    while (number >= value) {
+      result += numeral;
+      number -= value;
+    }
+  }
+
+  return result;
+}
 
 module.exports = convertToNewRoman;
