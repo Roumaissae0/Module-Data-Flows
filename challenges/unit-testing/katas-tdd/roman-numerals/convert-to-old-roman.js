8cde9d7b204afa941cf5ba14678d5f4fcb31edf3
Roumaissae <alroumaissaebb@gmail.com>
2026-06-13 22:45:20 +0200

    problem fixed

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,19 +1,26 @@
-function convertToOldRoman(n) { const values = [
+function convertToOldRoman(n) {
+  const values = [
     [1000, "M"],
+    [900, "CM"],
     [500, "D"],
+    [400, "CD"],
     [100, "C"],
+    [90, "XC"],
     [50, "L"],
+    [40, "XL"],
     [10, "X"],
+    [9, "IX"],
     [5, "V"],
+    [4, "IV"],
     [1, "I"],
   ];
 
   let result = "";
 
   for (const [value, numeral] of values) {
-    while (number >= value) {
+    while (n >= value) {
       result += numeral;
-      number -= value;
+      n -= value;
     }
   }
 
